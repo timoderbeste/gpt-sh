96ad2e27019ede6fc034e28cb87381d48cd9895f
timoderbeste <ntwang1994@gmail.com>
2023-03-26 19:21:32 -0500

    Publishing the basic version to pypi.

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 setup(
     name='shell-gpt2',
-    version='0.1',
+    version='0.1.3',
     description="A somewhat more advanced version of Shell GPT",
     author="Timo Wang",
     author_email="ntwang1994@gmail.com",
     packages=["."],
     entry_points={
         "console_scripts": [
-            "sgpt2 = sgpt2.main:main"
+            "sgpt2 = main:main"
         ],
     },
     install_requires=[
