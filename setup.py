5a21694fadec3bf78cfe1f8e85fc88c2170c9694
timoderbeste <ntwang1994@gmail.com>
2023-03-26 19:07:41 -0500

    Restructuring the project

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+setup(
+    name='shell-gpt2',
+    version='0.1',
+    description="A somewhat more advanced version of Shell GPT",
+    author="Timo Wang",
+    author_email="ntwang1994@gmail.com",
+    packages=["shell_gpt2"],
+    entry_points={
+        "console_scripts": [
+            "shell-gpt2 = main:main"
+        ],
+    },
+    install_requires=[
+        "shell_gpt",
+    ]
+)
