8bcf52d147fec575d086d9106c9b7e5b9588dcbe
timoderbeste <ntwang1994@gmail.com>
2023-03-27 12:19:34 -0500

    Adding longer description

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md") as fp:
 
 setup(
     name='shell-gpt2',
-    version='0.1.8',
+    version='0.2.0',
     description="A somewhat more advanced version of Shell GPT",
     long_description=long_description,
     author="Timo Wang",
