f91b8dc98c3447b7ebd9a3ff7a3c7aadd6f64e25
timoderbeste <ntwang1994@gmail.com>
2023-03-27 13:56:06 -0500

    Updating setup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md") as fp:
 
 setup(
     name='gpt-sh',
-    version='0.1.2',
+    version='0.1.3',
     description="A somewhat more advanced version of Shell GPT",
     long_description=long_description,
     long_description_content_type='text/markdown',
